b932318aff7815f0300376cb4fa5eef526d6b999
yuragms <yura.gms@gmail.com>
2026-01-15 06:55:04 +0200

    04-create-home-page-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,29 @@ const data = {
       href: '/page/help',
     },
   ],
+  carousels: [
+    {
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Sellers in T-Shirts',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
+    },
+  ],
 }
 
 export default data
